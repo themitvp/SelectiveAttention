5ab14d80277bffa04ab2b46c98e2686715172464
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2016-02-10 16:30:34 +0100

    Change between controllers added

--- ImageView.cs
+++ ImageView.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using UIKit;
+using CoreGraphics;
 
 namespace SelectiveAttention
 {
 	public class ImageView : UIView
 	{
 		// Subview properties
-		public UIButton seeOriginalBtn { get; set; }
+		public UIButton seeOriginalButton { get; set; }
 
-		public ImageView()
+		public ImageView(CGRect frame): base(frame)
 		{
+
+			this.BackgroundColor = UIColor.White;
+
+
+			seeOriginalButton = UIButton.FromType(UIButtonType.System);
+			seeOriginalButton.SetTitle("See Original", UIControlState.Normal);
+
+			var buttonWidth = 100;
+			var buttonHeight = 50;
+
+			seeOriginalButton.Frame = new CGRect(buttonWidth, frame.Height - buttonHeight*2, buttonWidth, buttonHeight);
+
+			this.AddSubview(seeOriginalButton);
 		}
 	}
 }
